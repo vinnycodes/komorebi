37d8c0d78e82a52cf93a75b78235d62c5ec1f60a
Vinny Nyamu <vincenyamu@gmail.com>
2020-09-01 23:48:24 -0400

    spelling correction of contact on nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
       <div className="nav__logo">KOMOREBI</div>
       <div className="nav__container">
         <ul className="nav__list">
-          <li className="nav__item">Contacts</li>
+          <li className="nav__item">Contact</li>
           <li className="nav__item">9104739010</li>
           <li className="nav__item">Reservation</li>
         </ul>
