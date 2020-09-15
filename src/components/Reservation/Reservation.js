91bd6dec4e61b572865136cedf2a9c6bef8b5e61
Vinny Nyamu <vincenyamu@gmail.com>
2020-09-15 18:47:14 -0400

    updated reservation form with validation

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Reservation.scss';
 
 const Reservation = () => {
+  const [date, setDate] = useState();
+  const [people, setPeople] = useState();
+  const [name, setName] = useState('');
+  const [specialWishes, setSpecialWishes] = useState('');
+  const [phone, setPhone] = useState('');
+  const [time, setTime] = useState('');
+
+  // Control Amount of People for Reservation
+  useEffect(() => {
+    // No lower than 0 people
+    if (people < 0) {
+      return setPeople(0);
+    }
+    // No higher than 21 people
+    if (people > 21) {
+      return setPeople(21);
+    }
+  }, [people]);
+
   return (
     <div className="reservation" id="reservation">
       <div className="reservation__container">
@@ -27,16 +46,50 @@ const Reservation = () => {
         </div>
         <div className="reservation__formContainer">
           <form action="" className="reservation__form">
-            <input type="text" name="date" placeholder="Date" />
-            <input type="text" name="time" placeholder="Time" />
+            <input
+              type="date"
+              min="2020-010-15"
+              max="2021-2-20"
+              required
+              name="date"
+              placeholder="Date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <input type="time" name="time" placeholder="Time" required />
             <input
               type="number"
               name="quantity"
-              placeholder="How many people"
+              placeholder={'How many people?'}
+              min="12:00"
+              max="24:00"
+              required
+              value={people}
+              onChange={(e) => setPeople(e.target.value)}
             />
-            <input type="text" name="wishes" placeholder="Special Wishes" />
-            <input type="text" name="name" placeholder="Your name" />
-            <input type="tel" name="phone" placeholder="Your Phone" />
+            <input
+              type="text"
+              name="wishes"
+              placeholder="Special Wishes"
+              required
+              value={specialWishes}
+              onChange={(e) => setSpecialWishes(e.target.value)}
+            />
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Your Phone"
+              required
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            />
             <input type="submit" value="Reservation" />
           </form>
           <div className="reservation__hours">
